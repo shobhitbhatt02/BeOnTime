cf000762f3ec9ed37a77357d2b663553f78d6c6b
shobhitbhatt02 <shobhitbhatt.sb@gmail.com>
2020-06-21 10:59:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/beontime/Home.java
@@ -0,0 +1,14 @@
+package com.example.beontime;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/example/beontime/Users.java
@@ -0,0 +1,14 @@
+package com.example.beontime;
+public class Users {
+    public String fullname,phonenumber,email,gender;
+    public Users(){
+
+    }
+    public Users(String fullname, String phonenumber, String email, String gender) {
+        this.fullname = fullname;
+        this.phonenumber = phonenumber;
+        this.email = email;
+        this.gender = gender;
+    }
+}
+
